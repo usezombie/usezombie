90b2f42beddd0a5dc2808a1e6c560cf9fa49b658
Kishore Kumar <kishore.kumar@e2enetworks.com>
2026-03-08 14:25:58 +0530

    chore(app): remove stale tracked app .next artifacts after package move

--- app/.next/dev/server/middleware.js
+++
@@ -1,6 +0,0 @@
-var R=require("./chunks/[turbopack]_runtime.js")("server/middleware.js")
-R.c("server/chunks/node_modules_next_headers_e5e3e7fa.js")
-R.c("server/chunks/node_modules_5aeddc0b._.js")
-R.c("server/chunks/[root-of-the-server]__b3e506bf._.js")
-R.m("[project]/node_modules/next/dist/esm/build/templates/middleware.js { INNER_MIDDLEWARE_MODULE => \"[project]/proxy.ts [middleware] (ecmascript)\" } [middleware] (ecmascript)")
-module.exports=R.m("[project]/node_modules/next/dist/esm/build/templates/middleware.js { INNER_MIDDLEWARE_MODULE => \"[project]/proxy.ts [middleware] (ecmascript)\" } [middleware] (ecmascript)").exports
